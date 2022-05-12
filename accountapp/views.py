162b82d16c63307598f3d5eec1e13127914a5eed
matt700395 <matt7003@korea.ac.kr>
2022-05-13 06:01:41 +0900

    httpResponseRedirect, reverse, objects.all

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,8 @@
 # accountapp/views.py
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -10,9 +13,9 @@ def hello_world(request):
 
         new_hello_world = HelloWorld()# 모델의 클래스로 인스턴스 생성
         new_hello_world.text = temp# 인스턴스 메소드에 temp로 받은 데이터를 넣음| temp는 위에서 post로 template에서 input name으로 전달받은 데이터
-        new_hello_world.text_model_test = temp
         new_hello_world.save() # db에 저장
 
-        return render(request, 'accountapp/hello_world.html',context={'hello_world_output': new_hello_world})#객체를 내보냄
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html',context={'text': 'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})

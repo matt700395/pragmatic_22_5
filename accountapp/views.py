af7482f901a6ff2a7371efd528b38c6882736285
matt700395 <matt7003@korea.ac.kr>
2022-05-13 05:23:43 +0900

    view, template & saving data

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,13 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+
+        # 모델의 클래스로 인스턴스 생성
+        new_hello_world = HelloWorld()
+
+        # 인스턴스 메소드에 temp로 받은 데이터를 넣음| temp는 위에서 post로 template에서 input name으로 전달받은 데이터
+        new_hello_world.text = temp
+
+        # db에 저장
+        new_hello_world.save()
 
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD'})
+                      context={'hello_world_output': new_hello_world})#객체를 내보냄
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD'})

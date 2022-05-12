ae5c006716dfd6237ab88b7564421be89853f7e3
matt700395 <matt7003@korea.ac.kr>
2022-05-13 05:44:59 +0900

    edit

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,6 +3,4 @@ from django.db import models
 # Create your models here.
 
 class HelloWorld(models.Model):
-    text_test_0001init = models.CharField(max_length=255, null=False)
-    text_2 = models.CharField(max_length=100, null=True)
-
+    text= models.CharField(max_length=255, null=False)
--- accountapp/urls.py
+++ accountapp/urls.py
@@ -7,6 +7,3 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name = 'hello_world')
 ]
-
-
-#127.0.0.1.:8000/accoiunt/hello_world
